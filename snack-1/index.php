27f7075bd8a2e7d61010220eb3f78fd1b035844e
FraAccaBi <francescahuda@gmail.com>
2022-05-20 12:54:38 +0200

    add var dump to chech results commit

--- snack-1/index.php
+++ snack-1/index.php
@@ -4,11 +4,29 @@ Olimpia Milano - Cantù | 55-60
  -->
 <?php
 $Grottammare = [
-    'squadra1' => 'Ascoli',
-    'squadra2' => 'SBT',
-    'punteggio1' => 2,
-    'punteggio2' => 3
+    'Ascoli',
+    'SBT',
+     2,
+     3,
 ];
-var_dump($Grottammare['squadra1']);
+
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php for ($i=0; $i < 4; $i++) { 
+    $element = ($Grottammare[$i]);
+    var_dump($element);
+}
+?>    
+    
+</body>
+</html>
