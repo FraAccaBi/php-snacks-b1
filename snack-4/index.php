d3a33bdbdfd12f2b0d5e249f051d6b74e4d7e36b
FraAccaBi <francescahuda@gmail.com>
2022-05-20 16:17:03 +0200

    complete fourth snack commit

--- snack-4/index.php
+++ snack-4/index.php
@@ -1,2 +1,16 @@
 <!-- ## Snack 4
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+<?php 
+
+$randomArray = [];
+
+while(count($randomArray) < 15) {
+    $number = rand(1,20);
+
+    if (!in_array($number, $randomArray) ) {
+        $randomArray[] = $number;
+    }
+}
+
+var_dump($randomArray);
