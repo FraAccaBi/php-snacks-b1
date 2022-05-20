14f4757e020b1aba80df33b3cab1fbec3236492b
FraAccaBi <francescahuda@gmail.com>
2022-05-20 12:42:06 +0200

    initial commit

---
+++ snack-1/index.php
@@ -0,0 +1,14 @@
+<!-- ## Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60
+ -->
+<?php
+$Grottammare = [
+    'squadra1' => 'Ascoli',
+    'squadra2' => 'SBT',
+    'punteggio1' => 2,
+    'punteggio2' => 3
+];
+var_dump($Grottammare['squadra1']);
+?>
+
