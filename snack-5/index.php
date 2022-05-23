b947273e5fba434262a955ee0c9ed99b72393599
FraAccaBi <francescahuda@gmail.com>
2022-05-23 12:17:36 +0200

    complete fifth snack commit

--- snack-5/index.php
+++ snack-5/index.php
@@ -3,7 +3,7 @@
 Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
  -->
 <?php
-$paragraph = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum dolorem quae libero dolore ad quia ullam fugit, possimus numquam dolor ipsam eius adipisci rerum quo cumque molestiae distinctio maiores, voluptatem qui corporis quisquam eaque officiis tempore. Nihil, ut aspernatur saepe voluptatibus sint eum officiis asperiores ea omnis rem expedita? Corporis quisquam ducimus iste quod porro obcaecati aut ipsum tenetur eum nesciunt eveniet voluptatibus quas sapiente nisi impedit debitis suscipit perspiciatis, omnis atque! Culpa ut saepe ipsa suscipit enim dolore numquam in laboriosam sunt omnis ducimus veritatis architecto sed velit inventore, a sit, est reiciendis non perspiciatis facere illum nam eveniet!';
+$paragraph = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolor, atque, quos numquam debitis doloremque rem, cumque quisquam aliquid accusamus vero itaque pariatur dolorem aspernatur reiciendis. Nisi velit, reprehenderit quidem nesciunt fugiat tempore accusamus, sapiente natus eveniet vel cumque quos voluptate voluptatem harum pariatur. Vitae aliquam expedita quod velit, dolorem iure reiciendis, a culpa repudiandae est accusantium quae ut praesentium itaque sit ex magni amet ullam temporibus cumque voluptate voluptates. Ut repellat maxime earum officiis quam, recusandae fugiat nostrum voluptatum beatae nihil molestias aperiam delectus eum accusamus rerum atque consectetur natus? Debitis incidunt quo quia quam, expedita culpa, laudantium voluptatem perferendis corporis, est blanditiis! Tenetur consequuntur molestias recusandae expedita omnis et at, dignissimos ipsa deleniti ducimus perspiciatis impedit reprehenderit provident similique accusamus, veritatis nostrum nam minus earum aliquam, nisi reiciendis eveniet quae. Qui exercitationem provident aut unde at temporibus expedita, repellendus, ipsam voluptas molestias rem commodi, quisquam fugiat delectus similique veniam. Quia iusto sequi quod voluptatibus non eos minus temporibus eaque veniam eveniet, deserunt laboriosam facilis dignissimos distinctio nobis ratione enim cumque aut iure consequatur repudiandae quisquam porro recusandae! Quia, eum aspernatur sed facilis maiores necessitatibus nostrum eius minima omnis consequuntur, eaque quaerat mollitia neque, libero laudantium? Odio, perferendis maxime.';
 
 $paragraphs = explode('.', $paragraph);
 var_dump($paragraphs[2]);
@@ -12,7 +12,7 @@ var_dump($paragraphs[2]);
 ?>
 
 
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -21,10 +21,11 @@ var_dump($paragraphs[2]);
     <title>Document</title>
 </head>
 <body>
-    
+    <?php foreach ($paragraphs as $paragraphs => $paragraph) : ?>
     <p>
-
+    <?php echo $paragraph ?>
     </p>
-
+    <?php endforeach ?>
+    
 </body>
-</html> -->
+</html>
