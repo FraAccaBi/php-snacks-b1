43ebf4e9b38ce9010cc66fd73733cbd3ccc9b627
FraAccaBi <francescahuda@gmail.com>
2022-05-23 12:13:02 +0200

    backup commit

--- snack-5/index.php
+++ snack-5/index.php
@@ -2,3 +2,29 @@
 
 Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
  -->
+<?php
+$paragraph = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum dolorem quae libero dolore ad quia ullam fugit, possimus numquam dolor ipsam eius adipisci rerum quo cumque molestiae distinctio maiores, voluptatem qui corporis quisquam eaque officiis tempore. Nihil, ut aspernatur saepe voluptatibus sint eum officiis asperiores ea omnis rem expedita? Corporis quisquam ducimus iste quod porro obcaecati aut ipsum tenetur eum nesciunt eveniet voluptatibus quas sapiente nisi impedit debitis suscipit perspiciatis, omnis atque! Culpa ut saepe ipsa suscipit enim dolore numquam in laboriosam sunt omnis ducimus veritatis architecto sed velit inventore, a sit, est reiciendis non perspiciatis facere illum nam eveniet!';
+
+$paragraphs = explode('.', $paragraph);
+var_dump($paragraphs[2]);
+
+
+?>
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <p>
+
+    </p>
+
+</body>
+</html> -->
